59da0d71689c56884646625f2ad73c2ac9b02e94
Fuchise Kayo <1201779@st.asojuku.ac.jp>
2014-06-12 12:30:37 +0900

    6/11 12

--- top.php
+++ top.php
@@ -6,40 +6,24 @@
 <link rel="stylesheet" href="nile.css">
 </head>
 <body>
-<h1>top</h1>
-
-<div id="icon">
-<a href="index.php"><img src="image/nile.png"></a>
+<div id="left">
+    <div id="icon">
+    	<a href="index.php"><img src="image/nile.png"></a>
+    </div>
+    <div id="search">
+            <form method="get" action="search.php">
+            <input type="text" id="searchform" name="search" />
+            <input type="submit" name="submit" value="検索" /></br>
+            </form>
+    </div>
 </div>
-<div id="search">
-		<form method="get" action="search.php">
-		<input type="text" id="searchform" name="search" />
-		<input type="submit" name="submit" value="検索" /></br>
-		</form>
-</div>
-<div id="etc">
+<div id="right">
 	<ul>
-		<li><a href="#"><img src="image/coin.png"><br>コイン</a></li>
-		<li><a href="#"><img src="image/会員情報.png"><br>会員情報</a></li>
-		<li><a href="#"><img src="image/カート.jpg"><br>カート</a></li>
-		<li><a href="#"><img src="image/購入履歴.jpg"><br>購入履歴</a></li>
-		<li><a href="#"><img src="image/help.jpg"><br>ヘルプ</a></li>
-	</ul>
-</div>
-
-
-<div id="top">
-	<ul>
-		<li><a href="index.php"><img src="image/nile.png"></a></li>
-		<li><form method="get" action="search.php">
-			<input type="text" id="search" name="search" />
-			<input type="submit" name="submit" value="検索" /></br>
-			</form></li>
-		<li><a href="#"><img src="image/coin.png"><br>コイン</a></li>
-		<li><a href="#"><img src="image/会員情報.png"><br>会員情報</a></li>
-		<li><a href="#"><img src="image/カート.jpg"><br>カート</a></li>
-		<li><a href="#"><img src="image/購入履歴.jpg"><br>購入履歴</a></li>
-		<li><a href="#"><img src="image/help.jpg"><br>ヘルプ</a></li>
+	  <li><a href="#"><img src="image/coin.png"><br>コイン</a></li>
+	  <li><a href="#"><img src="image/会員情報.png"><br>会員情報</a></li>
+	  <li><a href="#"><img src="image/カート.jpg"><br>カート</a></li>
+	  <li><a href="#"><img src="image/購入履歴.jpg"><br>購入履歴</a></li>
+	  <li><a href="#"><img src="image/help.jpg"><br>ヘルプ</a></li>
 	</ul>
 </div>
 
@@ -51,7 +35,5 @@
 		<li><a href="#">Tennis</a></li>
 	</ul>
 </div>
-
-
 </body>
 </html>
