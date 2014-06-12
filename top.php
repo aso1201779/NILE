3c113e1b663b3e970304aef10629c06c081ab606
Fuchise Kayo <1201779@st.asojuku.ac.jp>
2014-06-12 10:51:25 +0900

    6/12

--- top.php
+++ top.php
@@ -8,16 +8,40 @@
 <body>
 <h1>top</h1>
 
-<ul>
-	<li><a href="index.php"><img src="image/nile.png"></a></li>
-	<li><form method="get" action="search.php">
-		<input type="text" id="search" name="search" />
+<div id="icon">
+<a href="index.php"><img src="image/nile.png"></a>
+</div>
+<div id="search">
+		<form method="get" action="search.php">
+		<input type="text" id="searchform" name="search" />
 		<input type="submit" name="submit" value="検索" /></br>
-		</form></li>
-	<li><a href="#"><img src="image/coin.png"></a></li>
-	<li><a href="#"><img src="image/会員情報.png"></a></li>
-</ul>
+		</form>
+</div>
+<div id="etc">
+	<ul>
+		<li><a href="#"><img src="image/coin.png"><br>コイン</a></li>
+		<li><a href="#"><img src="image/会員情報.png"><br>会員情報</a></li>
+		<li><a href="#"><img src="image/カート.jpg"><br>カート</a></li>
+		<li><a href="#"><img src="image/購入履歴.jpg"><br>購入履歴</a></li>
+		<li><a href="#"><img src="image/help.jpg"><br>ヘルプ</a></li>
+	</ul>
+</div>
 
+
+<div id="top">
+	<ul>
+		<li><a href="index.php"><img src="image/nile.png"></a></li>
+		<li><form method="get" action="search.php">
+			<input type="text" id="search" name="search" />
+			<input type="submit" name="submit" value="検索" /></br>
+			</form></li>
+		<li><a href="#"><img src="image/coin.png"><br>コイン</a></li>
+		<li><a href="#"><img src="image/会員情報.png"><br>会員情報</a></li>
+		<li><a href="#"><img src="image/カート.jpg"><br>カート</a></li>
+		<li><a href="#"><img src="image/購入履歴.jpg"><br>購入履歴</a></li>
+		<li><a href="#"><img src="image/help.jpg"><br>ヘルプ</a></li>
+	</ul>
+</div>
 
 <div id="menu">
 	<ul>
